9760463ab4ca507486edc9691d9d8b57a69575d3
Ahmet Sari <106359636+eeeng@users.noreply.github.com>
2022-08-03 12:29:48 -0400

    Update views.py

--- entry3/mysite/myapp/views.py
+++ entry3/mysite/myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    HttpResponse("HEY RTM AHMET SARI PROJECT TO ULAK HABERLESME")
+    HttpResponse("HEY RTM PROJECT TO ENRTIES")
