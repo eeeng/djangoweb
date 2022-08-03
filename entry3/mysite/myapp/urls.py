676f82aca8450b8414531f1372c39c2b7b80a951
Ahmet Sari <106359636+eeeng@users.noreply.github.com>
2022-08-03 12:31:20 -0400

    Update urls.py

--- entry3/mysite/myapp/urls.py
+++ entry3/mysite/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+#from django.urls import include
 
 urlpatterns=[
     path('', views.index, name='index'),
+    #path('admin/', include(case1.html)),
 ]
